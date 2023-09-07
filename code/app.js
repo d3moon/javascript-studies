6ab6476b98932fecf9596058392a2fb8d573ce8f
Your Name <you@example.com>
2023-09-07 15:36:08 -0300

    sending...

--- code/app.js
+++ code/app.js
@@ -1,4 +1,9 @@
-const a = 1
-const b = '1'
-
-console.log(a === b)
+const hora = 11
+if (hora >= 12 && hora <= 17) {
+  console.log('Boa tarde');
+}
+else if (hora >= 18) {
+  console.log('Boa noite');
+} else {
+  console.log('Bom dia');
+}
