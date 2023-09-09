473181eef15569ad0c484a02a69f5c27e355d705
PurpleGhost <dinniecass@gmail.com>
2023-09-09 00:41:55 -0300

    done..

---
+++ app.js
@@ -0,0 +1,14 @@
+// const string = 'Essa é uma string'
+// const number = 10 //número inteiro(int)
+// const number2 = 1.5 //número decimal(float)
+// const booleanT = true 
+// const booleanF = false 
+// const undefined = undefined //indefinido 
+// const nulo = null //objeto
+
+let nome = "Rafaella"
+let idade = 16 
+ 
+let mensagem = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`
+
+console.log(mensagem)
