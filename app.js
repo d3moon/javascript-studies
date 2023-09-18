3fa9f411ce487fb2005fa4b0cf69a714a448e9c5
Tate <johnnywinchest3r@gmail.com>
2023-09-18 19:18:46 -0300

    sending...

---
+++ app.js
@@ -0,0 +1,11 @@
+const number = 1
+
+console.log(typeof(number.toString()))
+
+const decimal = 2.5
+
+console.log(decimal.toFixed())
+
+const textoMaiusculo = 'HELLO WORLD'
+
+console.log(textoMaiusculo.toLowerCase())
